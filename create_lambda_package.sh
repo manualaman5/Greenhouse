617d941c451bab5158c6bac5f4449a99cfa4ebc1
malaman5 <manumaristas5@gmail.com>
2020-06-03 22:00:33 -0400

    Script actualizado

--- create_lambda_package.sh
+++ create_lambda_package.sh
@@ -2,10 +2,16 @@
 BASEDIR=$(pwd)
 
 rm -rf $BASEDIR/lambda_package.zip
-cd  $BASEDIR/lambda/testenv/lib/python3.6/site-packages/
-zip -r9 $BASEDIR/lambda_package.zip *
-cd $BASEDIR/lambda/testenv/lib64/python3.6/site-packages/
-zip -r9 $BASEDIR/lambda_package.zip *
-cd $BASEDIR/lambda
 
-zip -9 $BASEDIR/lambda_package.zip test_lambda.py
+cd  $BASEDIR/lambda_v2/virtualenv/lib/python3.7/site-packages/
+zip -r9 $BASEDIR/lambda_package.zip *
+
+cd $BASEDIR/lambda_v2/virtualenv/lib64/python3.7/site-packages/
+zip -r9 $BASEDIR/lambda_package.zip *
+
+cd $BASEDIR/lambda_v2
+zip -g $BASEDIR/lambda_package.zip test_lambda.py
+
+#Update lambda function
+cd $BASEDIR
+aws lambda update-function-code --function-name pythontest --zip-file fileb://lambda_package.zip
