56b97e3cbe0129e0341081e508a38b485ef8fafa
malaman5 <manumaristas5@gmail.com>
2020-07-07 20:56:57 -0400

    Updated Run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -xv
 
+if  [ ! -d lambda/virtualenv ];
+then
+  cd lambda/
+  python3 -m venv virtualenv
+  source virtualenv/bin/activate
+  pip install requirements.txt
+  deactivate
+  cd ..
+fi
+
 cd Scripts/
 
 bash create_lambda_package.sh
